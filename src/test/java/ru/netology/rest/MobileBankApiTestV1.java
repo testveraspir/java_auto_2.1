fe18ea2e90b7e6762bf36daf40a8565027b7c703
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-05-11 13:05:20 +0300

    Update MobileBankApiTest3

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,18 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
+
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+
+
+        ;
     }
+
+
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -18,13 +18,14 @@ class MobileBankApiTestV3 {
       .when()
           .get("/demo/accounts")
       // Проверки
-      .then()
-          .statusCode(200)
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
+              .then()
+              .statusCode(200)
+              // специализированные проверки - лучше
+              .contentType(ContentType.JSON)
+              .body("", hasSize(3))
+              .body("[0].currency", equalTo("RUB"))
+              .body("[1].currency", equalTo("USD"))
+              .body("[0].balance", greaterThanOrEqualTo(0))
       ;
     }
 }
